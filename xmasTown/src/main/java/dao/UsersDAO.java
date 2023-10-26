be4fb08acb6663d02446e48c11562a6db7d06311
chun-jpn <138347444+chun-jpn@users.noreply.github.com>
2023-10-26 15:04:57 +0900

    UsersDAO修正

--- xmasTown/src/main/java/dao/UsersDAO.java
+++ xmasTown/src/main/java/dao/UsersDAO.java
@@ -5,9 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
+import model.Login;
 import model.UserAccount;
 
 public class UsersDAO {
@@ -15,37 +14,39 @@ public class UsersDAO {
 	private final String DB_user = "root";
 	private final String DB_pass = "";
 	
-//	全ての情報をカラムごとにリストに格納
-	public List<UserAccount> findAll(){
-		List<UserAccount>userAccountList = new ArrayList<>();
-//		JDBCドライバを読み込む
+	public UserAccount findByLogin(Login login) {
+		UserAccount account = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		}catch(ClassNotFoundException e) {
 			throw new IllegalStateException("JDBCドライバを読み込めませんでした");
 		}
+		
 //		データベースに接続
 		try(Connection conn = DriverManager.getConnection(JDBC_url, DB_user, DB_pass)){
-//			select文の準備(IDを大きい順に並べる
-			String sql = "select * from users order by user_id ask";
+//			select文の準備
+			String sql = "select * from accounts where mail=? and password=?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, login.getMail());
+			pStmt.setString(2, login.getPassword());
 //			結果表を取得
 			ResultSet rs = pStmt.executeQuery();
-//			//SELECT文の結果をArrayListに格納
-			while(rs.next()) {
-				int userId = rs.getInt("user_id");
-				String userName = rs.getString("user_name");
-				String address = rs.getString("address");
-				int tel= rs.getInt("tel");
-				String mail= rs.getString("mail");
-				String password= rs.getString("password");
-				UserAccount userAccount = new UserAccount(userId, userName, address, tel, mail, password);
-				userAccountList.add(userAccount);
+			
+			if(rs.next()) {
+//			ユーザーが存在したらデータ取得
+//			そのユーザーが表すAccountインスタンスを生成
+			String userName = rs.getString("user_name");
+			String address = rs.getString("address");
+			int tel = rs.getInt("tel");
+			String mail = rs.getString("mail");
+			String password = rs.getString("password");
+			
+			account = new UserAccount(userName, address,tel, mail, password);
 			}
 		}catch(SQLException e){
 			e.printStackTrace();
 			return null ;
 		}
-		return userAccountList;
+		return account;
 	}
 }
