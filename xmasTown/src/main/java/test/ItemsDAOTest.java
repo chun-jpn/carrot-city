c68cc81df75486b92b1fe6832d6c4c38fdf3737d
fg-95 <kana1220yoko@gmail.com>
2023-10-27 15:31:44 +0900

    ItemsDAO・テスト作成

---
+++ xmasTown/src/main/java/test/ItemsDAOTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.util.List;
+
+import dao.ItemsDAO;
+import model.Items;
+
+
+
+public class ItemsDAOTest {
+	public static void main(String[] args) {
+		
+		ItemsDAO itemsDAO = new ItemsDAO();
+		List<Items> itemsList = itemsDAO.findAll();
+		
+		
+		for(Items items : itemsList) {
+			System.out.println("ID:" + items.getItem_id());
+			System.out.println("カテゴリー:" + items.getCategory());
+			System.out.println("商品名:" + items.getItem_name());
+			System.out.println("価格:" + items.getPrice());
+			System.out.println("コメント:" + items.getComment());
+			System.out.println("仕入先:" + items.getRelease_flag());
+			System.out.println("登録日:" + items.getAdd_date());
+			System.out.println("更新日:" + items.getRewrite_date());
+			System.out.println("画像:" + items.getPicture());
+			
+		}
+	}
+}
