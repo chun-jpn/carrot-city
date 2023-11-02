7e4b97aac6c15e96bb1883a4a4fcfe03c0e0d337
chun-jpn <138347444+chun-jpn@users.noreply.github.com>
2023-11-02 11:50:01 +0900

    addItem機能を追加

--- xmasTown/src/main/java/servlet/AddItemServlet.java
+++ xmasTown/src/main/java/servlet/AddItemServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Items;
+
 /**
  * Servlet implementation class AddItemServlet
  */
@@ -19,6 +21,26 @@ public class AddItemServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/addItem.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String category = request.getParameter("category");
+		String itemName = request.getParameter("itemName");
+		int price = Integer.parseInt(request.getParameter("price"));
+		String comment = request.getParameter("comment");
+		int quantity = Integer.parseInt(request.getParameter("quantity"));
+		int releaseFlag = Integer.parseInt(request.getParameter("releaseFlag"));
+		String picture = request.getParameter("picture");
+//		Itemsインスタンスの生成
+		Items items = new Items(category, itemName, price, comment, quantity, releaseFlag, picture);
+//		リクエストスコープに保存
+		request.setAttribute("item", items);
+		// フォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/addItemConfirm.jsp");
 		dispatcher.forward(request, response);
 	}
 }
