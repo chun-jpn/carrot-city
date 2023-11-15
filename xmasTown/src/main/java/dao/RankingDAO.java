bb50152cc8d456241378d61ac44a21b30d44a2fd
chun-jpn <138347444+chun-jpn@users.noreply.github.com>
2023-11-15 15:03:36 +0900

    管理者用css追加

---
+++ xmasTown/src/main/java/dao/RankingDAO.java
@@ -0,0 +1,57 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Items;
+
+public class RankingDAO {
+	private final String JDBC_url = "jdbc:mysql://localhost:3306/xmas_town?charaxterEncoding=UTF-8&serverTimezone=JST";
+	private final String DB_user = "root";
+	private final String DB_pass = "";
+	
+	public List<Items> getRanking() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		}catch(ClassNotFoundException e) {
+			throw new IllegalStateException("JDBCドライバを読み込めませんでした");
+		}
+		
+		List<Items> rankingList = new ArrayList<>();
+
+		try (Connection conn = DriverManager.getConnection(JDBC_url, DB_user, DB_pass)) {
+			String sql = "SELECT items.item_id, items.item_name, items.price, items.picture, SUM(orders.quantity) AS total_quantity "
+						+ "FROM orders "
+						+ "JOIN items ON orders.item_id = items.item_id "
+						+ "GROUP BY items.item_id "
+						+ "ORDER BY total_quantity DESC "
+						+ "LIMIT 5";
+
+			try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+				try (ResultSet rs = pstmt.executeQuery()) {
+					while (rs.next()) {
+						int itemId = rs.getInt("item_id");
+						String itemName = rs.getString("item_name");
+						int price = rs.getInt("price");
+						String picture = rs.getString("picture");
+						int totalQuantity = rs.getInt("total_quantity");
+
+						Items item = new Items(itemId, itemName, price, picture, totalQuantity);
+
+						rankingList.add(item);
+					}
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return rankingList;
+    }
+	
+}
--- xmasTown/src/main/webapp/WEB-INF/jsp/changeAdminAccount.jsp
+++ xmasTown/src/main/webapp/WEB-INF/jsp/changeAdminAccount.jsp
@@ -3,8 +3,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>CHRISTMAS TOWN</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="format-detection" content="telephone=no">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/responsive.css">
+	<link rel="stylesheet" href="css/style_formframe.css">
+	<link rel="stylesheet" href="css/style_admin.css">
+	<title>CHRISTMAS TOWN</title>
 </head>
 <body>
 <h1>管理者のユーザー情報変更完了</h1>
